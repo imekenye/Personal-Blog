bdb69618311925234942cb7b7ab82845ad3fdfea
imekenye <ismekenye@gmail.com>
2019-04-27 07:22:14 -0700

    update init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 
-
-
 db = SQLAlchemy()
-
 
 
 login_manager = LoginManager()
@@ -18,8 +15,11 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
 
+
+
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
     # initializing flask extensions
     # db.app = app
     db.init_app(app)
@@ -29,10 +29,21 @@ def create_app(config_name):
 
 
     # Registering blueprints
+
+    from app.blog_posts.views import blog_posts
+    app.register_blueprint(blog_posts)
+
+    from app.users.views import users
+    app.register_blueprint(users)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
+
+
+
 
     return app
