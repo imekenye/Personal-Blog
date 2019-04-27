ce314e3e9f060a778bee2c0442d947dc89164799
imekenye <ismekenye@gmail.com>
2019-04-27 15:48:29 -0700

    update

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_script import Manager,Server
 from app.models import User,BlogPost,Quote
 from  flask_migrate import Migrate, MigrateCommand
 
+
 # Creating app instance
 app = create_app('production')
 
