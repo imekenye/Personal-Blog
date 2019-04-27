10329c7389990ec32118fe8402b15faf69078edb
imekenye <ismekenye@gmail.com>
2019-04-27 07:28:53 -0700

    create views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,13 @@
 from flask import  render_template,redirect,request,url_for,abort,flash
 from . import main
+from app.models import BlogPost
 
 @main.route('/')
 def index():
     '''
-    View root page function that returns the index page and its data
+    This is the home page view. Notice how it uses pagination to show a limited
+    number of posts by limiting its query size and then calling paginate.
     '''
-
-    title = 'Blogr - Welcome to my blog'
-
-    return render_template('index.html', title=title)
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html',blog_posts=blog_posts)
