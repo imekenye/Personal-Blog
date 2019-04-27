93f23d7ffc55837236fe2b3753b01150c6e4d0d5
imekenye <ismekenye@gmail.com>
2019-04-27 07:24:12 -0700

    update config file

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    CSRF_ENABLED = True
+
+
+
 
 
 
