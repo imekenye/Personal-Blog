ac6b662086aa74abaa2b5bbf028f0f2fa8f85b42
imekenye <ismekenye@gmail.com>
2019-04-27 15:17:01 -0700

    initial

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:nathangwaro17@localhost/blog'
