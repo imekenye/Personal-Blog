e6bd2b57bd6575e4853521c50f44503cae5859a6
imekenye <ismekenye@gmail.com>
2019-04-27 20:13:24 -0700

    add quotes

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,17 @@
 from flask import  render_template,redirect,request,url_for,abort,flash
 from . import main
 from app.models import BlogPost
+from ..requests import get_quotes
 
 @main.route('/')
 def index():
     '''
-    This is the home page view. Notice how it uses pagination to show a limited
-    number of posts by limiting its query size and then calling paginate.
+    This is the home page view.
     '''
+    myquote = get_quotes()
+    quote = myquote['quote']
+    quote_author = myquote['author']
+
     page = request.args.get('page', 1, type=int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
-    return render_template('index.html',blog_posts=blog_posts)
+    return render_template('index.html',blog_posts=blog_posts,quote = quote,quote_author = quote_author)
--- app/requests.py
+++ app/requests.py
@@ -1,45 +1,12 @@
-import urllib.request,json
-from .models import Quote
-
-# Getting api key
-# Getting the movie base url
-base_url = None
-
-def configure_request(app):
-    global base_url
-
-    base_url = app.config['QUOTE_API_BASE_URL']
+import requests
 
 def get_quotes():
     '''
-    Function that gets the json response to our url request
+    Fetches and returns random quotes from api
     '''
-    get_quotes_url = base_url.format()
 
-    with urllib.request.urlopen(get_quotes_url) as url:
-        get_quotes_data = url.read()
-        get_quotes_response = json.loads(get_quotes_data)
+    url = 'http://quotes.stormconsultancy.co.uk/random.json'
+    response = requests.get(url)
+    quote = response.json()
 
-        quote_results = None
-
-        if get_quotes_response['results']:
-            quote_results_list = get_quotes_response['results']
-            quote_results = process_results(quote_results_list)
-
-
-    return quote_results
-
-def process_results(quote_list):
-
-    quote_results = []
-    for quote_item in quote_list:
-        id = quote_item.get('id')
-        quote = quote_item.get('quote')
-        author = quote_item.get('author')
-
-        quote_object = Quote(id,quote,author)
-        quote_results.append(quote_object)
-
-    return quote_results
-
-
+    return quote
